ed2cc6182717cd969212634ea66866f9255bc2c4
Arseny Shishaev <ashishae@student.42.fr>
2021-10-14 16:11:39 +0200

    Finish auth

--- back/main.go
+++ back/main.go
@@ -1,17 +1,56 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/encryptcookie"
 )
 
 const redirect_url string = "http://127.0.0.1:3000/auth"
+
+type AuthResponse struct { // AuthResponse has other fields, but for now we only use one
+	AccessToken string `json:"access_token"`
+	// TokenType    string `json:"token_type"`
+	// ExpiresIn    int    `json:"expires_in"`
+	// RefreshToken string `json:"refresh_token"`
+	// Scope        string `json:"scope"`
+	// CreatedAt    int    `json:"created_at"`
+}
+
+func getAuthToken(CLIENT_ID string, CLIENT_SECRET string, code string) (string, error) {
+	// Hit 42 api
+	resp, err := http.PostForm("https://api.intra.42.fr/oauth/token",
+		url.Values{
+			"grant_type":    {"authorization_code"},
+			"client_id":     {CLIENT_ID},
+			"client_secret": {CLIENT_SECRET},
+			"code":          {code},
+			"redirect_uri":  {redirect_url},
+			"state":         {"1234"}})
+
+	if err != nil {
+		return "", err
+	}
+
+	// Read response as JSON
+	defer resp.Body.Close()
+
+	authResp := new(AuthResponse)
+	err = json.NewDecoder(resp.Body).Decode(authResp)
+
+	if err != nil {
+		return "", err
+	}
+
+	return authResp.AccessToken, nil
+}
 
 func main() {
 	CLIENT_ID := os.Getenv("CLIENT_ID")
@@ -26,10 +65,15 @@ func main() {
 
 	app := fiber.New()
 
+	// Default encrypted cookie middleware config
+	app.Use(encryptcookie.New(encryptcookie.Config{ // this re-creates keys each time
+		Key: encryptcookie.GenerateKey()})) // later we should use a random, but stable value
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		c.Set(fiber.HeaderContentType, fiber.MIMETextHTML) // to display as html
+
 		return c.SendString(`
-			<h1>Hello, World!</h1>
+			<h1>Hello, World! Backend speaking</h1>
 			<a href="/login">42 login demo</a>`)
 	})
 
@@ -41,34 +85,64 @@ func main() {
 
 	// Get code and trade it for auth token
 	app.Get("/auth", func(c *fiber.Ctx) error {
+		// Capture 42 redirect with auth code in query
 		code := c.Query("code")
 		if code == "" {
 			return c.SendString("Please get back with the code")
 		}
 
-		resp, err := http.PostForm("https://api.intra.42.fr/oauth/token",
-			url.Values{
-				"grant_type":    {"authorization_code"},
-				"client_id":     {CLIENT_ID},
-				"client_secret": {CLIENT_SECRET},
-				"code":          {code},
-				"redirect_uri":  {redirect_url},
-				"state":         {"1234"}})
+		// Exchange the code for an auth token
+		authToken, err := getAuthToken(CLIENT_ID, CLIENT_SECRET, code)
 
 		if err != nil {
-			fmt.Print("err: ", err)
-			c.SendString("42 api call failed")
+			return c.SendString("42 api call failed")
 		}
-		//We Read the response body on the line below.
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		//Convert the body to type string
-		sb := string(body)
-		log.Print(sb) // We get the access token here, need to read as json
 
-		return c.SendString(fmt.Sprint("Your code is: ", code))
+		// Save token in encrypted cookie
+		c.Cookie(&fiber.Cookie{
+			Name:  "42session",
+			Value: authToken})
+
+		c.Set(fiber.HeaderContentType, fiber.MIMETextHTML) // to display as html
+
+		return c.SendString("Auth successful! Try it out: <a href=\"/getMe\">Get my info</a>")
+	})
+
+	// A route that just returns /me from 42 api with our cookie token
+	app.Get("/getMe", func(c *fiber.Ctx) error {
+
+		// Get token back from encrypted cookies
+		token := c.Cookies("42session")
+		if token == "" {
+			return c.SendString("Please log in!")
+		}
+
+		// Get 'me' from 42 api
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", "https://api.intra.42.fr/v2/me", nil)
+
+		if err != nil {
+			return c.SendString("Couldn't create request")
+		}
+
+		req.Header.Add("Authorization", fmt.Sprint("Bearer ", token))
+
+		resp, err := client.Do(req)
+
+		if err != nil {
+			return c.SendString("42 api request failed")
+		}
+
+		// Read response
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			return c.SendString("Couldn't read 42 api response")
+		}
+
+		// Show the primitive json
+		return c.SendString(string(body))
 	})
 
 	app.Listen(":3000")
